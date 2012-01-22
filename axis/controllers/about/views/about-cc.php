bdf4cb125fe1fe2d830ff7ed7bc563b78cf9a57c
Eric Simons <ericsimons@es40.net>
2012-01-21 22:24:15 -0800

    latest uwt

--- axis/controllers/about/views/about-cc.php
+++ axis/controllers/about/views/about-cc.php
@@ -3,6 +3,10 @@ appHeader('About ClassConnect');
 ?>
 <div class="content"> 
 	<div class="row" style="padding:20px;padding-top:0px"> 
+<div style="font-family:Varela Round;font-size:18px;text-align:center;margin-top:7px;font-weight:bolder">
+	ClassConnect is a crazy simple way to organize, collaborate and share content.
+</div>
+
 
 	</div>
 </div>
--- axis/controllers/about/views/about-uwt.php
+++ axis/controllers/about/views/about-uwt.php
@@ -4,6 +4,27 @@ appHeader('United We Teach');
 <div class="content"> 
 	<div class="row" style="padding:20px;padding-top:0px"> 
 
+<div style="font-family:Varela Round;font-size:18px;text-align:center;color:#666;font-weight:bolder">
+	Every once in a while, a group of pioneers stray from the regular path and create the future.
+</div>
+<div style="font-family:Varela Round;font-size:18px;text-align:center;margin-top:7px;font-weight:bolder">
+	This is happening in education <span style="text-decoration:underline">right now</span>.
+</div>
+
+<div style="font-size:14px;margin-top:40px">
+A teacher's job isn't about managing their students. If that were the case, they would be called a managers, not teachers. A teacher's job is to put content into the hands of their students and make it come to life. But with state mandated management & hundreds of students, there simply aren't enough hours in the day to reach every student.
+<br /><br />
+This is about to change.
+<br /><br />
+We've seen how remarkable the results are when you put <a href="http://www.thedailyriff.com/articles/how-the-flipped-classroom-is-radically-transforming-learning-536.php" target="_blank">high quality content into the hands of a student.</a> There is a direction correlation between the quality of content and the quality of learning. Therefore, the best content has to succeed and be used in the classroom - because if it isn't, then the students won't succeed either.
+
+<br /><br />
+Teachers from around the world are working together to create, share, and enhance the best educational content to ensure high quality learning for their students. We want you to be a part of this incredible community because the only way we will win is if we all work together.
+
+<br /><br /> This is the turning point for education. United We Teach.
+</div> 
+
+
 	</div>
 </div>
 <?php
