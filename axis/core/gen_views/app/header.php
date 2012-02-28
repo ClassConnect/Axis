c294e9f1198f5ac70ab53b31cfa5c4462bc95dfa
Eric Simons <ericsimons@es40.net>
2012-02-28 10:34:46 -0800

    userID show ll and docs fix

--- axis/core/gen_views/app/header.php
+++ axis/core/gen_views/app/header.php
@@ -136,7 +136,7 @@ if ($notis['data'] != 0) {
               <a href="#" class="dropdown-toggle">Apps</a>
               <ul class="dropdown-menu">
                 <li><a href="/app/calendar">Calendar</a></li>
-                <?php if (user('id') <= 2700) { ?>
+                <?php if (user('id') <= 8342) { ?>
                 <li><a href="/app/livelecture">LiveLecture</a></li>
                 <li><a href="/app/docs">Docs</a></li>
                 <?php } ?>
