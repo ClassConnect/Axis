7ef9d7217147694a6894559a7bee1099de5c641c
Eric Simons <ericsimons@es40.net>
2012-02-28 05:40:46 -0800

    umm, lets see if its fixed now

--- axis/controllers/app/filebox/core/main.php
+++ axis/controllers/app/filebox/core/main.php
@@ -274,7 +274,7 @@ function updateDescendantForks($batchObj, $num, $single) {
 		if (!isset($single)) {
 			$rent_ids[] = array('parent.id' => (string) $tObj['_id']);
 			$rent_ids[] = array('parents.id' => (string) $tObj['_id']);
-			$elDesc[] = $tObj['_id'];
+			$elDesc[] = (string) $tObj['_id'];
 		}
 
 		$elUp[] = (string) $tObj['_id'];
