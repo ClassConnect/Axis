48b38dcb4836d5489a43c1ac4ad5249700e05bec
Eric Simons <ericsimons@es40.net>
2012-02-28 05:10:22 -0800

    fin?

--- axis/core/keep-alive/push.php
+++ axis/core/keep-alive/push.php
@@ -3,11 +3,7 @@ require_once('../coreInc.php');
 require_once('../../controllers/app/search/core/main.php');
 
 
-$client = initGearmanClient();
-$client->doBackground("pushDoc", '4f4cd0ecc58216550c000002');
 
-
-exit();
 global $mdb;
 $collection = $mdb->fbox_content;
 $data = $collection->find();
