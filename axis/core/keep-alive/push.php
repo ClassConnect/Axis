e6be71d3c8f0fc1967de94b5fd354669c7b2404a
Eric Simons <ericsimons@es40.net>
2012-02-28 02:12:08 -0800

    temp dump

---
+++ axis/core/keep-alive/push.php
@@ -0,0 +1,18 @@
+<?php
+require_once('../coreInc.php');
+require_once('../../controllers/app/search/core/main.php');
+
+
+global $mdb;
+$collection = $mdb->fbox_content;
+$data = $collection->find();
+
+foreach ($data as $obj) {
+	$docID = (string) $obj["_id"];
+	$client = initGearmanClient();
+	$client->doBackground("pushDoc", $docID);
+
+	echo "Pushed " . $docID . "\n";
+}
+
+?>
