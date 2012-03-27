7376807bd087ef39ac809356a32f6b6058ccb1f1
Eric Simons <ericsimons@es40.net>
2012-03-26 20:07:24 -0700

    enhanced stats

--- axis/controllers/app/stats/index.php
+++ axis/controllers/app/stats/index.php
@@ -4,6 +4,24 @@ $today = strtotime(date("d-m-Y"));
 
 $totToday = count(good_query_table("SELECT * FROM users WHERE reg_date >= $today AND (level = 3 OR level = 0)"));
 
-echo $totToday . '<br /><br />' . ($totTeachers - 2000) . '<br /><br />' . $totTeachers;
+echo 'SU today: ' . $totToday . '<br /><br />Since init: ' . ($totTeachers - 2000) . '<br /><br />Since origin: ' . $totTeachers;
+
+
+// crunch FBox nums
+
+global $mdb;
+
+// select a collection (analogous to a relational database's table)
+$collection = $mdb->fbox_content;
+$data = $collection->count();
+
+$params[] = array('permissions.type'=>3);
+$params[] = array('parentPermissions.type'=>3);
+$finalq = array('$or' => $params);
+$pubs = $collection->count($finalq);
+
+
+
+echo '<br /><br /><br /><br />fbox total: ' . $data . '<br /><br />pub total:' . $pubs;
 
 ?>
